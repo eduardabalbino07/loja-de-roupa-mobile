2ed828fb63dedb26c6c4e9fdce46d69af61119de
Eduarda Balbino <eduarda.balbino@academico.ifpb.edu.br>
2025-04-22 11:33:54 -0300

    Primeiros Testes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
   <Stack>
-    <Stack.Screen name= "index" options={{title: "Teste"}} />
+    <Stack.Screen name= "index" options={{title: "Organização da Loja"}} />
   </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import {Text, View, StyleSheet} from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      style={style.container}
+      >
+      <Text>Bem vindo!</Text>
     </View>
   );
 }
+
+
+const style = StyleSheet.create({
+  container: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  }
+})
+
