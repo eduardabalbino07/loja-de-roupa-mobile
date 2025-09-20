e8599de8fcb1999d652e585fc6bc466043ab03e5
eduardabalbino07 <eduarda.balbino@academico.ifpb.edu.br>
2025-09-20 17:34:57 +0000

    modificações

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from "expo-router";
 
+
 export default function RootLayout() {
   return (
       <Stack>
-      <Stack.Screen 
+      <Stack.Screen
         name="index"
         options={{ title: "" }}
       />
     </Stack>
   );
 }
+
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput} from "react-native";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
+       <View style={styles.inputContainer}>
         <Text style={styles.title}>Crie sua Conta</Text>
-        <Text style={styles.subtitle}>Nome Completo</Text>
-        <Text style={styles.subtitle}>Email</Text>
-        <Text style={styles.subtitle}>Senha</Text>
-      </View>
-    </View>
+        
+        <View style={styles.inputs}>
+        <TextInput style={styles.inputs} placeholder={'Nome Completo'}/>
+        <TextInput style={styles.inputs} placeholder={'Email'}/>
+        <TextInput style={styles.inputs} placeholder={'Senha'} secureTextEntry={true}/></View>
+
+         <TouchableOpacity style={styles.button} onPress={() => alert("Conta criada!")}>
+          <Text style={styles.buttonText}>Criar Conta</Text>
+        </TouchableOpacity>
+		</View>
+  </View>
+ </View>
   );
 }
 
@@ -18,7 +26,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
-    backgroundColor: "#b87fd3ff"
+    backgroundColor: "#B185BD"
   },
   main: {
     flex: 1,
@@ -29,10 +37,46 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 45,
     fontWeight: "bold",
-    color: "#fff"
+    color: "#fff",
+    padding: 8,  
+    marginBottom: 15,
   },
   subtitle: {
     fontSize: 26,
-    color: "#fff",
+    color: "#dbd3d3ff",
+    borderWidth: 2,
+    borderColor: "#fff",
+    padding: 8,  
+    marginBottom: 15,
+    borderRadius: 6,
   },
+
+  button: {
+    marginTop: 30,
+    backgroundColor: "#B185BD",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "#fff",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+inputs: {
+      borderWidth: 2,
+      borderColor: '#fff', 
+      marginBottom: 15,
+      width: 200,
+      paddingHorizontal: 20,
+     
+    },
+inputContainer: {
+    alignItems: "center",
+  }
+
 });
